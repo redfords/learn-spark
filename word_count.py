bbfdfd300f0f29600aebeefbbcb1d50286c10ba2
redfords <joanapiovaroli@gmail.com>
2022-08-31 23:40:00 -0300

    Add program layout

--- word_count.py
+++ word_count.py
@@ -5,6 +5,16 @@ spark = (SparkSession
         .appName("Analyzing the vocabulary of Pride and Prejudice.")
         .getOrCreate())
 
+# log-level keywords
+# off, fatal, error, warn, info, debug, trace, all
+
 spark.sparkContext.setLogLevel("KEYWORD")
+
+# mapping our program
+# 1. read the input data
+# 2. tokenize each word
+# 3. clean and remove punctuaton or non words, lowercase each word
+# 4. count the frequency of each word
+# 5. return the top 10 (or 20, 50, 100)
 
 book = spark.read.text("./data/gutenberg_books/1342-0.txt")
