551bebe3e67d42b88958655f906223a83b568b6a
redfords <joanapiovaroli@gmail.com>
2022-09-17 23:41:25 -0300

    Add exercises from chapter 2

--- 02_exercises.py
+++ 02_exercises.py
@@ -1,3 +1,25 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, length, greatest
+
 spark = SparkSession.builder.getOrCreate()
+
+# count the number of columns that aren't strings
+
+exo2_2_df = spark.createDataFrame(
+    [["test", "more test", 10_000_000_000]], ["one", "two", "three"]
+)
+
+# rewrite without the 
+
+exo2_3_df = (
+    spark.read.text("./data/gutenberg_books/1342-0.txt")
+    .select(length(col("value")))
+    .withColumnRenamed("length(value)", "number_of_char")
+)
+
+# return the greatest value using select and greatest
+
+exo2_4_df = spark.createDataFrame(
+    [["key", 10_000, 20_000]], ["key", "value1", "value2"]
+)
 
