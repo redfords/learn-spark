d91b1947f0eadded9baac913de85af1b9aae4248
redfords <joanapiovaroli@gmail.com>
2022-09-20 23:21:16 -0300

    Move to separate directory

--- word_count/word_count.py
+++ word_count/word_count.py
@@ -20,13 +20,6 @@ spark = (SparkSession
 
 spark.sparkContext.setLogLevel("ERROR")
 
-# mapping our program
-# 1. read the input data
-# 2. tokenize each word
-# 3. clean and remove punctuaton or non words, lowercase each word
-# 4. count the frequency of each word
-# 5. return the top 10 (or 20, 50, 100)
-
 book = spark.read.text("./data/gutenberg_books/1342-0.txt")
 
 print(book)
