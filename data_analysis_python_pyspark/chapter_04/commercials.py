a8c445f796e26d2bff806b1ca2959ee763b20746
redfords <joanapiovaroli@gmail.com>
2022-09-30 19:22:26 -0300

    Add spark reader

--- data_analysis_python_pyspark/chapter_04/commercials.py
+++ data_analysis_python_pyspark/chapter_04/commercials.py
@@ -1,3 +1,14 @@
 from pyspark.sql import SparkSession
+import os
 
 spark = SparkSession.builder.getOrCreate()
+
+DIRECTORY = "./data/broadcast_logs"
+
+logs = spark.read.csv(
+    path=os.path.join(DIRECTORY, "BroadcastLogs_2018_Q3_M8.CSV"),
+    sep="|",
+    header=True,
+    inferSchema=True,
+    timestampFormat="yyyy-MM-dd",
+)
